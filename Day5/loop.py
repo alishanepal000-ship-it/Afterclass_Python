47c2fc0400339cd9167a167ab3a7894a1b2fb0bd
alishanepal000-ship-it <alishanepal000@gmail.com>
2026-07-08 14:17:18 +0545

    Learned loop and nested loop

---
+++ Day5/loop.py
@@ -0,0 +1,14 @@
+for i in range(4):
+    pass #it passes to next step it does not print
+
+
+
+for i in range(1,9):
+    print('Ok')
+
+
+for i in range(0, 10, 2):
+    print(i)
+
+for i in range(5, 0, -1):
+    print(i)
---
+++ Day5/nestedloop.py
@@ -0,0 +1,20 @@
+# for row in range(1,4):
+#     for col in range(1,4):
+#         print(row* col)
+#     print()
+
+
+# for row in range(1,4):
+#     for col in range(1,4):
+#         print(row* col, end=' ')
+#     print()
+
+
+
+#matrix multiplicaiton:
+for i in range(1,4):
+    for j in range(1,4):
+        for k in range(1,4):
+            print(i*j*k, end=' ')
+        print()
+    print()
